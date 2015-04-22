eff9d93d3d0b9f0ee9db277aba3ca69ce07bea7b
brelsford <christa.brelsford@gmail.com>
2015-04-22 11:46:42 -0600

    cleaning and commenting

--- Example.py
+++ Example.py
@@ -2,11 +2,6 @@ import numpy as np
 import math
 from matplotlib import pyplot as plt
 
-#import sys
-#sys.path.append(r'/Users/christa/Dropbox/SDI/Christa/topology_paper/python')
-#sys.path.append('C:\Users\F75VD\Dropbox\SDI\Christa\Topology_paper\python')
-
-import my_graph as mg
 import my_graph_helpers as mgh
 
 
@@ -17,8 +12,8 @@ def define_capetown_barriers(myG):
     be2 = [e for e in be if e.nodes[1].x < 146 and e.nodes[1].x > 25]
 
     be3 = [e for e in be2 if e.nodes[0].y < 20 and e.nodes[1].y < 20]
-    todrop = [e for e in be3 if e.nodes[0].x > 25 and e.nodes[0].x < 75
-              and e.nodes[0].y > 13.4 and e.nodes[1].y > 13.4]
+    todrop = [e for e in be3 if e.nodes[0].x > 25 and e.nodes[0].x < 75 and
+              e.nodes[0].y > 13.4 and e.nodes[1].y > 13.4]
 
     for e in be3:
         if abs(e.rads) > math.pi/4:
@@ -36,16 +31,15 @@ def define_epworth_barriers(myG):
 
     be2 = [e for e in myG.myedges() if e.nodes[0].x > 100 and
            e.nodes[0].x < 113 and e.nodes[0].y > 119 and e.nodes[0].y < 140]
-    be3 = [e for e in be2 if e.nodes[1].x > 100 and e.nodes[1].x < 113
-           and e.nodes[1].y > 119 and e.nodes[1].y < 140]
+    be3 = [e for e in be2 if e.nodes[1].x > 100 and e.nodes[1].x < 113 and
+           e.nodes[1].y > 119 and e.nodes[1].y < 140]
 
     return be+be3[0:-1]
 
 
 if __name__ == "__main__":
-    # make false in order to not re-import all the files.  The rest of the code
-    # just runs based on original
-    if True:
+
+    if True:  # make false in order to not re-import all the files.
         place = 'epworth'
         if place == 'epworth':
             filename = "data/epworth_demo"
@@ -56,30 +50,38 @@ if __name__ == "__main__":
             rezero = np.array([-31900, -3766370])
             define_barriers = define_capetown_barriers
 
-        original = mgh.import_and_setup(0, filename, rezero=rezero)
+        original = mgh.import_and_setup(0, filename, rezero=rezero,
+                                        name=place+"_S0")
         original.define_roads()
         original.define_interior_parcels()
+        barrier_edges = define_barriers(original)
+        mgh.build_barriers(original, barrier_edges)
 
+    # copy original, so that we can re-run from a clean base
     myG = original.copy()
 
-    barrier_edges = define_barriers(myG)
-    mgh.build_barriers(myG, barrier_edges)
+    # plot roads, using the original, unedited version as master to color
+    # original roads
+    myG.plot_roads(master=original)
 
-    barriers = myG.copy()
+    # finds roads to connect all interior parcels
+    new_roads = mgh.build_all_roads(myG)
 
-    myG.plot_roads(master=barriers)
+    # plot new roads. original roads (black) defined by original graph.
+    myG.plot_roads(master=original, parcel_labels=False, new_plot=True)
 
-    # makes a new graph taking into accuont the barriers as defined above
-    barGraph = mgh.graphFromMyEdges(barrier_edges)
-
-    new_roads = mgh.build_all_roads(myG, barriers, bisect=False)
-
-    myG.plot_roads(barriers, parcel_labels=False, new_plot=True)
-
-    stacks = myG.stacked_duals()
-
-    plt.show()
-    
     # red: interior parcels, bold black: original roads, blue: new roads,
     # green: barriers
-    
+
+###############
+# Dual graphs: pretty not important for stamen
+###############
+
+    stack = myG.stacked_duals()
+    colors = ['grey', 'purple', 'blue', 'red', 'orange', 'yellow']
+    width = [1, 1, 2, 4, 6]
+    node_size = [2, 10, 25, 35, 45]
+    myG.plot_weak_duals(stack=stack, colors=colors, width=width,
+                        node_size=node_size)
+
+    plt.show()
